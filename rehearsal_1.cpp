6179b38463f411450ed14b72bc21673c36e06090
sathayu2544 <59278037+sathayu2544@users.noreply.github.com>
2020-01-10 17:26:18 +0700

    Update rehearsal_1.cpp

--- rehearsal_1.cpp
+++ rehearsal_1.cpp
@@ -1,1 +1,33 @@
+#include<iostream>
+#include<string>
+using namespace std;
 
+int main()
+{
+	int N;
+	cout << "What is your name?: ";
+	cin >> N;
+	cout << "What is your GPA?: ";
+	cin >> N;
+	
+	string name[N];
+	string GPA[N];
+	float x=0;
+	{
+		if(x>=3.50)
+			cout << "Name of student "; 
+			cin >> name[N];
+			cout << "GPA of student ";
+			cin >> GPA[N];
+			cout << N << "Inw Jrim Jrim!!!";
+	}
+	else
+	{
+			cout << "Name of student "; 
+			cin >> name[N];
+			cout << "GPA of student ";
+			cin >> GPA[N];
+			cout << "Try harder, "<< N <<" !!!";
+	}
+	return 0;
+}
